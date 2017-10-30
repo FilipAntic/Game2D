4445769b0e10d9bd4c15bee562b123b8e2d07b43
FilipH <antic.filip996@gmail.com>
2017-10-30 11:28:54 +0100

    Menja boju na prvoj slici kad je kliknuto unutar kvadrata

--- src/projekat1/MainFrame.java
+++ src/projekat1/MainFrame.java
@@ -2,10 +2,13 @@ package projekat1;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.util.Random;
+
+import javax.swing.Timer;
 
 import rafgfxlib.GameFrame;
 import rafgfxlib.Util;
@@ -13,8 +16,11 @@ import rafgfxlib.Util;
 public class MainFrame extends GameFrame {
 
 	final int[] ints = new Random().ints(0, 6).distinct().limit(6).toArray();
+	WritableRaster raster = Util.createRaster(600, 600, false);
 	int sirinaEkrana = getWidth();
+	BufferedImage image;
 	int visinaEkrana = getHeight();
+	boolean first = true;
 	Koordinate[] cords = { new Koordinate(0, 0), new Koordinate(150, 0), new Koordinate(300, 0), new Koordinate(450, 0),
 			new Koordinate(0, 150), new Koordinate(150, 150), new Koordinate(300, 150), new Koordinate(450, 150),
 			new Koordinate(0, 300), new Koordinate(150, 300), new Koordinate(300, 300), new Koordinate(450, 300),
@@ -23,11 +29,13 @@ public class MainFrame extends GameFrame {
 
 	public MainFrame() {
 		super("Projekat1 - Igra memorije", 1000, 800);
+		image = makeRaster();
 		startThread();
 	}
 
 	public static void main(String[] args) {
 		new MainFrame().initGameWindow();
+
 	}
 
 	@Override
@@ -59,6 +67,18 @@ public class MainFrame extends GameFrame {
 		int y = arg1 - ((visinaEkrana - 600) / 2);
 		if (x >= 0 && x < 600 && y >= 0 && y < 600) {
 			System.out.println("To je to");
+			int[] rgb = new int[3];
+
+			rgb[0] = 255;
+			rgb[1] = 0;
+			rgb[2] = 51;
+			for (int y1 = 0; y1 < 150; y1++) {
+				for (int x1 = 0; x1 < 150; x1++) {
+					raster.setPixel(x1, y1, rgb);
+				}
+			}
+			image = Util.rasterToImage(raster);
+			update();
 		}
 	}
 
@@ -76,8 +96,6 @@ public class MainFrame extends GameFrame {
 
 	@Override
 	public void render(Graphics2D arg0, int arg1, int arg2) {
-		arg0.setColor(Color.BLUE);
-		BufferedImage image = makeRaster();
 		int x = (sirinaEkrana - 600) / 2;
 		int y = (visinaEkrana - 600) / 2;
 		arg0.drawImage(image, x, y, null);
@@ -88,7 +106,7 @@ public class MainFrame extends GameFrame {
 	}
 
 	public BufferedImage makeRaster() {
-		WritableRaster raster = Util.createRaster(600, 600, false);
+
 		BufferedImage image = Util.loadImage("slika.png");
 		WritableRaster source = image.getRaster();
 		int[] rgb = new int[3];
@@ -104,7 +122,7 @@ public class MainFrame extends GameFrame {
 			int kordY = koordinata.y;
 			for (int y = kordY; y < kordY + 150; y++) {
 				for (int x = kordX; x < kordX + 150; x++) {
-					source.getPixel(x-kordX, y-kordY, rgb);
+					source.getPixel(x - kordX, y - kordY, rgb);
 					raster.setPixel(x, y, rgb);
 				}
 			}
